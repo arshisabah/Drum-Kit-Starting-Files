163b12e567ed45475b7ce07ca17a2e49c2bd8614
Arshi <arshisabah326@gmail.com>
2024-07-04 13:22:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+//DETECTING BUTTON PRESS
+
+for(let i = 0; i<document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var buttonInner = this.innerHTML;
+        
+        makeSound(buttonInner);
+        animation(buttonInner);
+
+    });
+
+}
+
+//DETECTING KEYBOARD PRESS
+
+addEventListener("keypress",function(event) {
+    makeSound(event.key);
+    animation(event.key);
+});
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "a" :
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "s" :
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "d" :
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case "j" :
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case "k" :
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case "l" :
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        default:
+            break;
+    }
+}
+
+function animation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    if (activeButton) {
+        activeButton.classList.add("pressed");
+        setTimeout(function() {
+            activeButton.classList.remove("pressed");
+        }, 100);
+    }
+}
